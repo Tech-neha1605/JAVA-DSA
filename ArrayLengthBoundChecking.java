1dcaca75f9144849ff044b9be30274a7c41cc861
Neha Patil <tech.neha1605@gmail.com>
2025-09-13 19:59:39 +0530

    Array Length Bound Checking

---
+++ ArrayLengthBoundChecking.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+public class ArrayLengthBoundChecking {
+	
+	public static void main(String[] args) {
+		
+		int[]num = {10,20,30,40,50};
+		int x = num.length;
+		
+		System.out.println("Length of an Array :" +x);
+		System.out.println(num[4]);
+	}
+
+}
