4a2fd196b7c08c712773cd7b870b16a3a53e4fe5
Neha Patil <tech.neha1605@gmail.com>
2025-09-26 18:58:27 +0530

    StringEx1

---
+++ StringExample1.java
@@ -0,0 +1,18 @@
+package com.tka;
+
+public class StringExample1 {
+	
+	public static void main(String[] args) {
+		char ch[]= {'N'  , 'E' , 'H' , 'A' };
+		String str1 = new String(ch);
+		
+		String str2 ="Neha";
+		
+		String str3 = new String("Neha Patil");
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+
+}
