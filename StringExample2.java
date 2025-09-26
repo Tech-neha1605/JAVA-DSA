b780768db9fbcea2623950f196e15641fce253a1
Neha Patil <tech.neha1605@gmail.com>
2025-09-26 18:58:59 +0530

    StringEx2

---
+++ StringExample2.java
@@ -0,0 +1,12 @@
+package com.tka;
+
+public class StringExample2 {
+	
+	public static void main(String[] args) {
+		
+		String str = "Neha";
+		str.concat("Patil");
+		System.out.println(str);
+	}
+
+}
