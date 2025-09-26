b4d7e8b25923bb9d3033d2382e906fa6a7732c21
Neha Patil <tech.neha1605@gmail.com>
2025-09-26 18:59:56 +0530

    StringUpdate

---
+++ UpdateString.java
@@ -0,0 +1,12 @@
+package com.tka;
+
+public class UpdateString {
+	
+public static void main(String[] args) {
+		
+		String str = "Neha";
+		str = str.concat("Patil");
+		System.out.println(str);
+	}
+
+}
